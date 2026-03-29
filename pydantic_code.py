018a561e5b6492f13ec0375c5629726f89a3602b
mdsaibhossain <saibhossain5@gmail.com>
2026-03-29 21:20:09 +0600

    pydantic

--- pydantic_code.py
+++ pydantic_code.py
@@ -1,19 +1,21 @@
 from pydantic import BaseModel, EmailStr, AnyUrl
-
+from pydantic import Field
 
 class Patient(BaseModel):
     name : str
     email: EmailStr
+    weight : float=Field(gt=0)
     age: int
 
 def insert_patient_date(patient: Patient):
     print(patient.name)
     print(patient.email)
+    print(patient.weight)
     print(patient.age)
     print('inserted')
 
 
-patient_info = {'name':'nitish','email':'saib@gmail.com','age':'30'}
+patient_info = {'name':'nitish','email':'saib@gmail.com','weight':'1', 'age':'30'}
 
 patient1 = Patient(**patient_info)
 
