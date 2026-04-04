a065c30416e0121d60df6a6019d7e2ef2343ae71
mdsaibhossain <saibhossain5@gmail.com>
2026-04-05 01:56:13 +0600

    4th project start

--- Project4_ai_inference_api/app/main.py
+++ Project4_ai_inference_api/app/main.py
@@ -1,10 +1,17 @@
 from fastapi import FastAPI
+from app.routes.auth import router as auth_router
+from app.routes.users import router as users_router
+from app.routes.predict import router as predict_router
 
 app = FastAPI(
     title="Project 4 - AI Inference API",
     description="AI inference API with JWT authentication and prediction history",
     version="1.0.0",
 )
+
+app.include_router(auth_router, prefix="/auth", tags=["Auth"])
+app.include_router(users_router, prefix="/users", tags=["Users"])
+app.include_router(predict_router, prefix="/predict", tags=["Prediction"])
 
 
 @app.get("/")
